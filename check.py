e27c8eb1f944360e6aa09ba5d9dcae3385bf3357
Hujisawaraito <50525968+Hujisawaraito@users.noreply.github.com>
2019-10-04 16:17:56 +0900

    Update check.py

--- check.py
+++ check.py
@@ -1,9 +1,35 @@
-v={"tank":{"c1":{"helth":600,"dps":30},"c2":{"helth":500,"dps":20}},"damege":{"c3":{"helth":200,"dps":80}}}
+class chr_data():
+    def __init__(self):
+        self.name=""
+        self.helth=0.0
+        self.dps=0.0
+        self.Armor=0.0
+        self.kaifuku=0.0
+
+
+#v={"tank":{"c1":{"helth":600,"dps":30},"c2":{"helth":500,"dps":20}},"damege":{"c3":{"helth":200,"dps":80}}}
 #print(v)
 #print(v["tank"])
-t=v["tank"]
-print(t.values())
-k=t["c1"]
-t=k.values()
-for i in t:
-    print(i)
+#t=v["tank"]
+#print(t.values())
+#k=t["c1"]
+#t=k.values()
+list_helth={"D.va":200,"WINSTON":400}
+list_dps={"D.va":200,"WINSTON":400}
+list_Armor={"D.va":200,"WINSTON":400}
+list_kaifuku={"D.va":200,"WINSTON":400}
+def nyuryoku():
+    tankx=chr_data()
+    tankx.name=input("D.vaを入力してください:")
+    tankx.helth=list_helth[tankx.name]
+    tankx.dps=list_dps[tankx.name]
+    if tankx.name in list_Armor.keys():
+        tankx.Armor=list_Armor[tankx.name]
+
+    if tankx.name in list_kaifuku.keys():
+        tankx.kaifuku=list_kaifuku[tankx.name]
+    return tankx.name,tankx.helth,tankx.dps,tankx.Armor,tankx.kaifuku
+tankA=nyuryoku()
+
+#tankB=nyuryoku()
+print(tankA)
