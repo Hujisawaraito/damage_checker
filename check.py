7421c0ca9bdaa2c235d563026267958c13249f56
Hujisawaraito <50525968+Hujisawaraito@users.noreply.github.com>
2019-10-04 15:02:36 +0900

    Update check.py

--- check.py
+++ check.py
@@ -1,2 +1,9 @@
-a=0
-b=0
+v={"tank":{"c1":{"helth":600,"dps":30},"c2":{"helth":500,"dps":20}},"damege":{"c3":{"helth":200,"dps":80}}}
+#print(v)
+#print(v["tank"])
+t=v["tank"]
+print(t.values())
+k=t["c1"]
+t=k.values()
+for i in t:
+    print(i)
